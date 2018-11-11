8cb7171cda9230e717ba028ed72f909d28b880ff
shadow002 <david.saldana.s@gmail.com>
2018-11-11 11:30:28 -0300

    modificado el userprofile para que muestre el nombre de usuario, automatizado para que el default de la creacion de usuario sea profile de adoptante, comentarios en html

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
 
 class UserProfile(models.Model):
@@ -7,3 +8,10 @@ class UserProfile(models.Model):
     tipo = models.CharField(max_length=20, default ="adoptante")
 
     
+    def __str__(self):
+        return self.user.username
+        
+    def create_profile(sender, **kwargs):
+        if kwargs['created']:
+            user_profile = UserProfile.objects.create(user = kwargs['instance'])
+    post_save.connect(create_profile, sender =User)
